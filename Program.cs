ef47ea4ac90248b312dba0167e7b029673f203dd
EmaMaga <135460319+EmaMaga@users.noreply.github.com>
2024-02-03 20:07:49 -0300

    Add files via upload
    Un bucle donde se comprueba si el numero de paquete que se quiere buscar existe

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Channels;
+namespace Curso_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //El numero ya se encuentra asociado a un paquete
+            int numeroPackete = 1234;
+            bool accept=false;
+            Console.WriteLine("Escriba su numero de paquete");
+            while (accept == false)
+            {
+                try
+                {
+                    int PN = Int32.Parse(Console.ReadLine());
+                    //Metodo que comprueba el numero puesto con el numero del paquete
+                    ComprovacionPN(PN, numeroPackete);                    
+                    accept= true;
+                }
+                catch (Exception e) when (e.GetType() != typeof(FormatException)) 
+                { Console.WriteLine("El numero que agrego es demaciado grande."); accept = false; }
+                catch (FormatException e)
+                {
+                    Console.WriteLine("Solo Numeros porfavor.");
+                    accept = false;
+                }
+            }
+
+        }
+        //Este seria el metodo
+        public static void ComprovacionPN(int PN, int numeroPackete)
+        {
+            while (PN != numeroPackete)
+            {
+                Console.WriteLine("Intentelo de nuevo");
+                PN =Int32.Parse(Console.ReadLine());
+                
+            }
+            Console.WriteLine("Codigo correcto");
+
+
+        }
+    }
+}
